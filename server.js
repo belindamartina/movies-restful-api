7e50813d7b82d671a848e3ef82e36323823cb00a
belindamartina <justcallmeby@gmail.com>
2026-02-10 06:04:22 +0700

    fixing html bug

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const path = require('path')
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.DATABASE_URL)
@@ -13,7 +14,7 @@ const db = mongoose.connection
 db.on('error', (error)=> console.error(error))
 db.once('open', ()=> console.log('connected to database'))
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 
 const moviesRouter = require('./routes/movies')
@@ -22,6 +23,10 @@ app.use('/auth', moviesRouter)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>console.log(`listening to port ${PORT}...`));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 
 module.exports = app
